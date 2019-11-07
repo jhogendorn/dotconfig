133a18f9c10738f8a4cf1c798ad453babb61c245
Joshua Hogendorn <jhogendorn@gmail.com>
2019-11-07 14:50:47 +1000

    add centering commands for hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,3 +10,66 @@ spoon.MiroWindowsManager:bindHotkeys({
   left = {hyper, "left"},
   fullscreen = {hyper, "f"}
 })
+
+-- center focused x and y
+hs.hotkey.bind(hyper, 'c', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.x = ((max.w - f.w) / 2) + max.x
+	x.y = ((max.h - f.h) / 2) + max.y
+	win:setFrame(x)
+end)
+
+-- center focused y
+hs.hotkey.bind(hyper, 'x', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.y = ((max.h - f.h) / 2) + max.y
+	win:setFrame(x)
+end)
+
+-- center focused x
+hs.hotkey.bind(hyper, 'v', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.x = ((max.w - f.w) / 2) + max.x
+	win:setFrame(x)
+end)
+
+-- maximize vertically
+hs.hotkey.bind(hyper, '[', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.y = max.y
+	x.h = max.h
+	win:setFrame(x)
+end)
+
+-- maximize horizontally
+hs.hotkey.bind(hyper, ']', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.x = max.x
+	x.w = max.w
+	win:setFrame(x)
+end)
